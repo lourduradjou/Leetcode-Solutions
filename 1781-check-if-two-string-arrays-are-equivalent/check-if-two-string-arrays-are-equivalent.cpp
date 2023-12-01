6b76c1f8cc3b09174cd8d03d633bc6340ffab04b
Lourdu Radjou <rajlourdu15@gmail.com>
2023-12-02 00:08:27 +0530

    Time: 4 ms (57.15%) | Memory: 12.1 MB (31.59%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        string s1, s2;
+        int i = 0, j = 0;
+        int ite_of_word1 = 0, ite_of_word2 = 0;
+        int n1 = word1.size(), n2 = word2.size();
 
-        for(auto ch : word1) {
-            s1 += ch;
+        while(i < n1 && j < n2) {
+            string s1 = word1[i];
+            string s2 = word2[j];
+            int size_of_S1 = s1.size();
+            int size_of_S2 = s2.size();
+
+            while(ite_of_word1 < size_of_S1 && ite_of_word2 < size_of_S2) {
+                if(s1[ite_of_word1] != s2[ite_of_word2])
+                    return false;
+                ite_of_word1++;
+                ite_of_word2++;
+            }
+            if(ite_of_word1 == size_of_S1) {
+                i++;
+                ite_of_word1 = 0;
+            }
+            if(ite_of_word2 == size_of_S2) {
+                j++;
+                ite_of_word2 = 0;
+            }
         }
 
-        for(auto ch: word2) {
-            s2 += ch;
-        }
-
-        if(s1 == s2)
-            return true;
-        return false;
+        if(i != n1 || j != n2) 
+            return false;
+        
+        return true;
     }
 };

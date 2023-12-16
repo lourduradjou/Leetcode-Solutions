37c3fe036dd74988a887da4b8b8522a2bde207c2
Lourdu Radjou <rajlourdu15@gmail.com>
2023-12-16 13:30:26 +0530

    Time: 29 ms (61.40%) | Memory: 13.7 MB (40.26%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -16,7 +16,7 @@ public:
             for(int col = 0; col < m; col++) {
                 if( !vis[row][col] && grid[row][col] == '1' ) {
                     count++;
-                    bfs(grid, row, col, vis);               
+                    dfs(grid, row, col, vis);               
                 }
 
             }
@@ -25,37 +25,27 @@ public:
         return count;
     }
 
-    void bfs( vector<vector<char>> &grid, int row, int col, vector<vector<int>> &vis) {
+    void dfs( vector<vector<char>> &grid, int row, int col, vector<vector<int>> &vis) {
         vis[row][col] = 1;
-        queue<pair<int, int>> queue;
-        queue.push({row, col});
         int n = grid.size();
         int m = grid[0].size();
 
-        while( !queue.empty() ) {
-            int row = queue.front().first;
-            int col = queue.front().second;
+        for(int inRow = -1; inRow <= 1; inRow++) {
+            for(int inCol = -1; inCol <= 1; inCol++) {
+                
+                int newRow = row + inRow;
+                int newCol = col + inCol;
 
-            queue.pop();
-
-            for(int inRow = -1; inRow <= 1; inRow++) {
-                for(int inCol = -1; inCol <= 1; inCol++) {
-                    
-                    int newRow = row + inRow;
-                    int newCol = col + inCol;
-
-                    if(inRow != 0 && inCol != 0) 
-                        continue;
-                    //in this problem we gotta check only the four directions 
-                    //so check that only when both of the inrow and incol are not 
-                    //equal to zero means it is diagonal direction so skip it.
-                    if( newRow >= 0 && newRow < n && newCol >= 0 && 
-                    newCol < m && !vis[newRow][newCol] && grid[newRow][newCol] == '1' ) {
-                        vis[newRow][newCol] = 1;
-                        queue.push({newRow, newCol});
-                    }            
-                }                
-            }
+                if(inRow != 0 && inCol != 0) 
+                    continue;
+                //in this problem we gotta check only the four directions 
+                //so check that only when both of the inrow and incol are not 
+                //equal to zero means it is diagonal direction so skip it.
+                if( newRow >= 0 && newRow < n && newCol >= 0 && 
+                newCol < m && !vis[newRow][newCol] && grid[newRow][newCol] == '1' ) {
+                    dfs(grid, newRow, newCol, vis);
+                }            
+            }                
         }
     }
 };

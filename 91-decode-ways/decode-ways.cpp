8aba0505ecf3e5b9a8b53bd9938db2b39ceff2da
Lourdu Radjou <rajlourdu15@gmail.com>
2023-12-25 12:52:57 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (87.76%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int solve(int i, string &s, int &n, vector<int> &dp){
+        if(i == n){
+            return 1; // out of bound (>26)
+        }
+        if(s[i] == '0'){
+            return 0;  // np split
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        int ans = solve(i+1, s, n, dp);
+
+        if(i+1 < n){          // overflow check
+            if(s[i] == '1' || (s[i] == '2' && s[i+1] <= '6')){   // 1_ or <=26 condition check
+                ans += solve(i+2, s, n, dp);
+            }   
+        }
+        return dp[i] = ans;
+    }
+
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n+1, -1);
+
+        return solve(0, s, n, dp);
+    }
+};

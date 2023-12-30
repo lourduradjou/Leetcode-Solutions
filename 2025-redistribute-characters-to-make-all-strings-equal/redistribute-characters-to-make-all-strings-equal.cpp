d234d43b1e3774bda4121ecd482e38e6a3cccf7e
Lourdu Radjou <rajlourdu15@gmail.com>
2023-12-30 14:31:09 +0530

    Time: 24 ms (7.78%) | Memory: 12.6 MB (21.01%) - LeetSync

---
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        //use a vector to count the number of each chars in each string
+        //and then just compare whether it has a count of n 
+        int n = words.size();
+        if(n == 1)
+            return true;
+        unordered_map<char,int> freq;
+        for(string word : words) {
+            for(char ch: word) {
+                freq[ch]++;
+            }
+        }
+
+        for(auto it: freq) {
+            if(it.second % n != 0)
+                return false;
+        }
+
+        return true;
+    }
+};

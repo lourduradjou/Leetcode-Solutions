8c354369d246ff33751865384b16b231c40b533a
Lourdu Radjou <rajlourdu15@gmail.com>
2023-12-30 16:52:09 +0530

    Time: 49 ms (5.11%) | Memory: 14.4 MB (58.40%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+                 //we are using topological sort method to schedule 
+    //it is just if there is a cycle we can't form or can form thats all here i am using bfs 
+    //in dfs just use a stack it is also easy
+    int V = numCourses;
+    vector<int> adj[V];
+    for(auto it: prerequisites) {
+        adj[it[1]].push_back(it[0]);
+    }
+    
+    vector<int> inDegree(V, 0);
+    for(int i = 0; i < V; i++) {
+        for(auto it: adj[i])
+            inDegree[it]++;
+    }
+    
+    queue<int> q;
+    for(int i = 0; i < V; i++) {
+        if(inDegree[i] == 0)
+            q.push(i);
+    }
+    
+    vector<int> ans;
+    while(!q.empty()) {
+        int node = q.front();
+        q.pop();
+        ans.push_back(node);
+        
+        for(auto adjNode: adj[node]) {
+            inDegree[adjNode]--;
+            if(inDegree[adjNode] == 0)
+                q.push(adjNode);
+        }
+    }
+    
+    // cout<<V<<endl;
+    if(ans.size() == V)
+        return ans;
+    else 
+        return {};
+    }
+};

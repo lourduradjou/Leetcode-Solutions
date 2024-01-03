3357fd500f012866c773aef0a122906d30c95079
Lourdu Radjou <rajlourdu15@gmail.com>
2024-01-03 06:41:53 +0530

    Time: 81 ms (8.85%) | Memory: 43.4 MB (34.21%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -5,10 +5,7 @@
 var compose = function(functions) {
     
     return function(x) {
-        for(let i = functions.length-1; i >= 0; i--) {
-            x = functions[i](x)
-        }
-        return x;
+       return functions.reduceRight((acc, fn) => fn(acc), x);
     }
 };
 

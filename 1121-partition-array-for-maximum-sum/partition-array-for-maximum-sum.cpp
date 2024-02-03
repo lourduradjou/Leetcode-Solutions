39ff5a4bdb99a38a0c15c43930e28dc18b625192
Lourdu Radjou <rajlourdu15@gmail.com>
2024-02-03 20:37:32 +0530

    Time: 11 ms (74.73%) | Memory: 11 MB (14.40%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,26 +1,17 @@
 class Solution {
-private:
-    vector<int> dp;
-    int n, k;
-
-    int solve(int ind, vector<int> &arr) {
-        if(ind >= n) return 0;
-
-        if(dp[ind] != -1) return dp[ind];
-
-        int maximumValue = 0, ans = 0;
-        for(int j = ind; j < min(n, ind + k); j++) {
-            maximumValue = max(maximumValue, arr[j]);
-            ans = max(ans, (j - ind + 1) * maximumValue + solve(j + 1, arr));
-        }
-
-        return dp[ind] = ans;
-    }
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        n = arr.size();
-        this->k = k;
-        dp = vector<int> (n, -1);
-        return solve(0, arr);
+        int n = arr.size();
+        vector<int> dp(n+1, 0);
+
+        for(int ind = n-1; ind >= 0; ind--) {
+            int maxValue = 0;
+            for(int j = ind; j < min(n, ind + k); j++) {
+                maxValue = max(maxValue, arr[j]);
+                dp[ind] = max(dp[ind], (j - ind + 1) * maxValue + dp[j+1]);
+            }
+        }
+
+        return dp[0];
     }
 };

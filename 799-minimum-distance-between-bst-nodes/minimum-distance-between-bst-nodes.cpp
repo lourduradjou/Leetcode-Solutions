3cd7bc26af24d1372589aabb7512035c63ea7b70
Lourdu Radjou <rajlourdu15@gmail.com>
2024-02-07 00:36:11 +0530

    Time: 5 ms (30.04%) | Memory: 11.5 MB (6.10%) - LeetSync

---
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+private:
+    void inorder_traversel(TreeNode* root, vector<int> &arr) {
+        if(root == NULL) return;
+        inorder_traversel(root->left, arr);
+        arr.push_back(root->val);
+        inorder_traversel(root->right, arr);
+    }
+public:
+    int minDiffInBST(TreeNode* root) {
+        //binary search tree will give sorted values in inorder traversal
+        vector<int> arr;
+        inorder_traversel(root, arr);
+        int n = arr.size();
+        int minValue = INT_MAX;
+
+        for(int i = 0; i < n-1; i++) {
+            minValue = min(minValue, arr[i+1] - arr[i]);
+        }
+
+        return minValue;
+    }
+};

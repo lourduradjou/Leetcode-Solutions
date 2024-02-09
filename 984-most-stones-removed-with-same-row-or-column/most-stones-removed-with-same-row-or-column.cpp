7fe6eac17b449629c605a849704c2df27f3a6284
Lourdu Radjou <rajlourdu15@gmail.com>
2024-02-09 17:28:08 +0530

    Time: 36 ms (73.64%) | Memory: 25 MB (15.21%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,86 @@
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+                // Code here
+        //very simple solution but kinda looks different
+        //in the initial view ..but literally easy ..
+        int n = stones.size();
+        int maxRow = 0;
+        int maxCol = 0;
+        for(auto it: stones) {
+            maxRow = max(maxRow, it[0]);
+            maxCol = max(maxCol, it[1]);
+        }
+        //we found row and col size here ..
+        DisjointSet ds(maxRow + maxCol + 2);
+        //plus 2 to avoid both row and col index accessing
+        unordered_map<int, int> stonesFound;
+        
+        for(auto it: stones) {
+            int stoneRow = it[0];
+            int stoneCol = it[1] + maxRow + 1;
+            ds.unionBySize(stoneRow, stoneCol);
+            stonesFound[stoneRow] = 1;
+            stonesFound[stoneCol] = 1;
+        }
+        
+        int cnt = 0;
+        for(auto pos: stonesFound) {
+            if(ds.findUPar(pos.first) == pos.first)//it is the parent of itself 
+            //so we found a component so lets increase the cnt
+                cnt++;
+        }
+        return n - cnt;
+    }
+};

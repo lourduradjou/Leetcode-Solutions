bc929f4569c763df8ba5f9726d0ba2554796b569
Lourdu Radjou <rajlourdu15@gmail.com>
2024-02-10 22:31:58 +0530

    Time: 10 ms (45.78%) | Memory: 15.4 MB (33.37%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,24 +1,25 @@
 class Solution {
 private:
-    vector<vector<int>> dp;
-    int isPalindrome(string s , int i , int j ) {
-        if(i >= j) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        return dp[i][j] = (s[i] == s[j]) ? isPalindrome(s, i+1, j-1) : 0;
-    }
-
-public:
-    int countSubstrings(string s) {
+    int palindrome(string s, int i , int j ) {
         int count = 0;
-        int n = s.size();
-        dp = vector<vector<int>> (n, vector<int> (n, -1));
-
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                count += isPalindrome(s, i, j);//;
-            }
+        while(i >= 0 && j < s.size() && s[i] == s[j]) {
+            i--;
+            j++;
+            count++;
         }
         return count;
     }
-}; 
+public:
+    int countSubstrings(string s) {
+        //another method to solve it 
+        //going from center to outwords 
+        //for both even and odd approaches of palindromes
+        int count = 0, n = s.size();
+        for(int i = 0; i < n; i++) {
+            int oddCount = palindrome(s, i , i);
+            int evenCount = palindrome(s, i, i+1);
+            count += (oddCount + evenCount);
+        }
+        return count;
+    }
+};

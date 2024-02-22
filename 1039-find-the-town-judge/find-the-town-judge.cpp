cea2c0e4cc2ceffaeb77f2e9204fe6f7110cf1e5
Lourdu Radjou <rajlourdu15@gmail.com>
2024-02-22 07:01:49 +0530

    Time: 146 ms (48.70%) | Memory: 72.5 MB (21.11%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        if(n == 1)
-            return 1;
-        
-        vector<int> trustCount(n + 1);
-
-        for(auto &person: trust) {
-            trustCount[person[0]]--;
-            trustCount[person[1]]++;
+        //if a person trust another person lets consider there
+        //is a edge from A to B ...
+        //though is to play with indegree and outdegree 
+        //the town judge wouldn't believe anyone so outdegree will be 0
+        //and all the ones will believe him so indegree will be n-1
+        vector<int> inDegree(n + 1) , outDegree(n + 1);
+        for (auto edge: trust) {
+            outDegree[edge[0]]++;
+            inDegree[edge[1]]++;
         }
 
-        for(int person = 0; person < trustCount.size(); person++) {
-            if( trustCount[person] == n - 1 ) //only if he is trusted by n - 1 peoples he is the judge
-            return person;
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == n-1 && outDegree[i] == 0)
+                return i;
         }
 
         return -1;

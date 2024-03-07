286c0ed5d81c2c1b6f8ceb39e0482c8a4bdd851e
Lourdu Radjou <rajlourdu15@gmail.com>
2024-03-07 20:28:57 +0530

    Time: 16 ms (94.79%) | Memory: 29.3 MB (33.50%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -9,11 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+
 class Solution {
+private:
+    int find_left_height(TreeNode* root) {
+        int height = 0;
+        while (root)
+        {
+            height++;
+            root = (root->left);
+        }
+        return height;
+    }
+
+    int find_right_height(TreeNode* root) {
+        int height = 0;
+        while (root)
+        {
+            height++;
+            root = (root->right);
+        }
+        return height;
+    }
 public:
     int countNodes(TreeNode* root) {
-        if (root == NULL)
+        if (root == NULL) 
             return 0;
+    
+        int left_height = find_left_height(root);
+        int right_height = find_right_height(root);
+        //cout<<left_height<<" "<<right_height<<endl;
+
+        if (left_height == right_height) 
+            return (1 << left_height) - 1;
         
         return 1 + countNodes(root->left) + countNodes(root->right);
     }

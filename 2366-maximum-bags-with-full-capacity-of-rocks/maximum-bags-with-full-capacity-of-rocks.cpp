6e01132a379a205b26442647e9ffd15700f31ace
Lourdu Radjou <rajlourdu15@gmail.com>
2024-04-11 14:48:18 +0530

    Time: 129 ms (64.01%) | Memory: 90.3 MB (74.40%) - LeetSync

--- 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
@@ -3,8 +3,6 @@ public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
         //this is kinda maximization (greedy) problems we have to maximize the count of
         //filled bags which contains full of rocks before or after addition of rocks in it
-        //
-
         //so we need to find the remaining rocks to be filled from the given values
         int n = rocks.size();
         vector<int> rocksRequired(n);

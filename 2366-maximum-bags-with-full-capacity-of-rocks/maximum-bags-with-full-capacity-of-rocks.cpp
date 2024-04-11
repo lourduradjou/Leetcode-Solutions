1ed5ebba241a118c811984b94f0c0f18285e9094
Lourdu Radjou <rajlourdu15@gmail.com>
2024-04-11 14:49:26 +0530

    Time: 133 ms (53.06%) | Memory: 90.3 MB (60.11%) - LeetSync

--- 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
@@ -18,6 +18,9 @@ public:
                 additionalRocks -= rocksRequired[i];
                 ans++;
             }
+            else {
+                break;
+            }
         }
 
         return ans;

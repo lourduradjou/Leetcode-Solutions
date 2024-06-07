5e5bfa31ad292d8ebf12ff533c584fa4b65a3c59
Lourdu Radjou <rajlourdu15@gmail.com>
2024-06-07 16:19:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (59.58%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,24 @@
 class Solution {
+private:
+    int solve(vector<int> &dp, vector<int> &nums, int n) {
+        if (n == 0) return nums[0]; //first house value
+        if (n < 0) return 0;
+
+        if (dp[n] != -1) return dp[n];
+
+        //pick or not pick method
+        int pick = nums[n] + solve(dp, nums, n-2);//we pick the house money so lets move 2 steps
+
+        int notPick = solve(dp, nums, n - 1);
+
+        return dp[n] = max(pick, notPick);
+    }
+
 public:
     int rob(vector<int>& nums) {
-        int size = nums.size();
-        if(size == 1) return nums[0];
-        vector<int> dp(size,0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
-
-        for(int i=2; i<size; i++) {
-            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
-        }
-        return dp[size-1];
+        //memo + subsequence method we can try
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return solve(dp, nums, n-1);
     }
 };

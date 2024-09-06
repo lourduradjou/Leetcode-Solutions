aecb03e3ac55554c4e7964426ebf1821e25ade7b
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-09-07 02:22:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (67.53%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,22 +1,20 @@
 class Solution {
-public:
-    vector<vector<int>> dp;
-    int findPaths(int i, int j) {
-        if(i == 0 && j == 0) {
-            return 1;
-        }
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
+private:
+    int solve(int i, int j , vector<vector<int>> &dp) {
+        if (i == 0 && j == 0) return 1;
+        if (i < 0 || j < 0) return 0;
 
-        int upCount = findPaths(i, j-1);
-        int leftCount = findPaths(i-1, j);
-        return dp[i][j] = (upCount + leftCount);
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int left = solve(i, j-1, dp);
+        int up = solve(i-1, j, dp);
+
+        return dp[i][j] = left + up;
     }
+public:
     int uniquePaths(int m, int n) {
-        //as recursion gives tle lets try bottom up ..
-        //so down and right here will change as up and left..
-        //so that at that point what are the possible moves we can find
-        dp = vector<vector<int>> (m+1, vector<int>(n+1, -1));
-        return findPaths(m-1, n-1);
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(m-1, n-1, dp);
     }
 };
+

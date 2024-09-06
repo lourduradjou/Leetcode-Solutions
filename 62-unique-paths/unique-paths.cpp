8b96ac499528fcc8ee32fb0871fd3d21bc770ad6
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-09-07 02:38:39 +0530

    Time: 4 ms (12.37%) | Memory: 8 MB (50.48%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,25 @@
 class Solution {
-private:
-    int solve(int i, int j , vector<vector<int>> &dp) {
-        if (i == 0 && j == 0) return 1;
-        if (i < 0 || j < 0) return 0;
 
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int left = solve(i, j-1, dp);
-        int up = solve(i-1, j, dp);
-
-        return dp[i][j] = left + up;
-    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(m-1, n-1, dp);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int up = 0, left = 0; 
+                    //we are taking the left and top just because fromt that
+                    //way only you could have get into the current point
+                    //so could their paths, simple!
+                    if (i > 0) up = dp[i-1][j];
+                    if (j > 0) left = dp[i][j-1];
+
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
 

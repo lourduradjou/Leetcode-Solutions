1271d0ebcf3e154125a1590128c5e602ebdd9bf7
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-09-07 17:56:33 +0530

    Time: 4 ms (74.26%) | Memory: 11.6 MB (68.00%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int solve(int i, int j , int m, vector<vector<int>> &dp, vector<vector<int>> &triangle) {
-        if (i == m-1) return triangle[i][j];
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int down = triangle[i][j] + solve(i+1, j, m, dp, triangle);
-        int diaMove = triangle[i][j] + solve(i+1, j+1, m, dp, triangle);
-
-        return dp[i][j] = min(down, diaMove);
-    }
 
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();;
+        int n = triangle.size();
 
-        vector<vector<int>> dp(m, vector<int>(m,-1));
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        for (int i = n-1; i >= 0; i--) {
+            dp[n-1][i] = triangle[n-1][i];
+        }
+        //we tried to go from top to bottom in memo with 0 to the bottom 
+        //in tabu it is the opposite 
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                if (i == n-1) dp[i][j] = triangle[i][j];
+                else {
+                    int down = triangle[i][j] + dp[i+1][j];
+                    int diaRight = triangle[i][j] + dp[i+1][j+1];
 
-        return solve(0,0, m, dp, triangle);
+                    dp[i][j] = min(down, diaRight);
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };

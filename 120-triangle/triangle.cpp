5f64ec29c96fb9729ec59f02765a481adf9427fb
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-09-07 17:59:55 +0530

    Time: 4 ms (74.26%) | Memory: 11.7 MB (56.31%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -4,24 +4,26 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
 
-        vector<vector<int>> dp(n, vector<int>(n,-1));
+        vector<int> front(n,0);
         for (int i = n-1; i >= 0; i--) {
-            dp[n-1][i] = triangle[n-1][i];
+            front[i] = triangle[n-1][i];
         }
         //we tried to go from top to bottom in memo with 0 to the bottom 
         //in tabu it is the opposite 
         for (int i = n-2; i >= 0; i--) {
+            vector<int> cur(i+1, 0);
             for (int j = i; j >= 0; j--) {
-                if (i == n-1) dp[i][j] = triangle[i][j];
+                if (i == n-1) cur[j] = triangle[i][j];
                 else {
-                    int down = triangle[i][j] + dp[i+1][j];
-                    int diaRight = triangle[i][j] + dp[i+1][j+1];
+                    int down = triangle[i][j] + front[j];
+                    int diaRight = triangle[i][j] + front[j+1];
 
-                    dp[i][j] = min(down, diaRight);
+                    cur[j] = min(down, diaRight);
                 }
             }
+            front = cur;
         }
 
-        return dp[0][0];
+        return front[0];
     }
 };

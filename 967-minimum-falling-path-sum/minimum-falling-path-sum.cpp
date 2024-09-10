b1bb29f6ef231ea406c669f47120ef7aaf8acc08
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-09-10 23:43:01 +0530

    Time: 16 ms (22.63%) | Memory: 13.6 MB (79.00%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -4,27 +4,29 @@ public:
         //here they asking for min rather than max, same 2d dp
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int> (n,-1));
+        vector<int> prev(n,-1);
         //initialization
         for (int j = 0; j < n; j++) {
-            dp[0][j] = matrix[0][j];
+            prev[j] = matrix[0][j];
         }
 
         for (int i = 1; i < m; i++) {
+            vector<int> cur(n, 0);
             for (int j = 0; j < n; j++) {
                 int leftDia = 1e8, rightDia = 1e8;
-                int up = matrix[i][j] + dp[i-1][j];
-                if(j > 0) leftDia = matrix[i][j] + dp[i-1][j-1];
-                if(j < n-1) rightDia = matrix[i][j] + dp[i-1][j+1];
+                int up = matrix[i][j] + prev[j];
+                if(j > 0) leftDia = matrix[i][j] + prev[j-1];
+                if(j < n-1) rightDia = matrix[i][j] + prev[j+1];
 
-                dp[i][j] = min({up, leftDia, rightDia});
+                cur[j] = min({up, leftDia, rightDia});
             }
+            prev = cur;
         }
 
         int mini = 1e8;
         
         for (int j = 0; j < n; j++) {
-            mini = min(mini, dp[m-1][j]);
+            mini = min(mini, prev[j]);
         }
 
         return mini;

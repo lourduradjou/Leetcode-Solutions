c7d6876b1bc2b45113492364ce70414ec20aeac3
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-09-13 03:11:30 +0530

    Time: 666 ms (24.80%) | Memory: 186.3 MB (26.08%) - LeetSync

--- 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.cpp
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    void setFreq(string word, vector<int> &freq) {
-
-        for (char ch: word) {
-            freq[ch-'a']++;
-        }
-    }
     int similarPairs(vector<string>& words) {
         int cnt = 0;
         for (int i = 0; i < words.size(); i++) {
-            vector<int> freq(26, 0);
-            setFreq(words[i], freq);
+            unordered_set<char> st1;
+            for(char ch: words[i]) st1.insert(ch);
 
             for (int j = i+1; j < words.size(); j++) {
+                unordered_set<char> st2;
                 int flag = 1;
                 for(char ch: words[j]) {
-                    if (freq[ch - 'a'] == 0) {
+                    st2.insert(ch);
+                    if (st1.find(ch) == st1.end()) {
                         flag = 0;
                         break;
                     }
                 }
-                unordered_set<char> st1, st2;
-                for(char ch: words[i]) st1.insert(ch);
-                for(char ch: words[j]) st2.insert(ch);
+                  
                 if (st1.size() == st2.size() && flag) 
                     cnt += flag;
-        
-                // if (flag) {
-                //     cout<<words[i]<<" "<<words[j]<<endl;
-                // }
             }
         }
 

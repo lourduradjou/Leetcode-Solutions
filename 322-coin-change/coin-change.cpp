c4f190bdb50241df779c1adf5941f649b834d0e0
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-09-23 22:43:39 +0530

    Time: 31 ms (63.47%) | Memory: 20.7 MB (48.83%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        vector<int> prev(amount + 1, 0), cur(amount + 1, 0);
         //tabulation
         //base case
         for (int target = 0; target <= amount; target++) {
-            if (target % coins[0] == 0) dp[0][target] = target / coins[0];
-            else dp[0][target] = 1e8;
+            if (target % coins[0] == 0) prev[target] = target / coins[0];
+            else prev[target] = 1e8;
         }
 
         for (int ind = 1; ind < n; ind++) {
             for (int target = 0; target <= amount; target++) {
-                int notPick = dp[ind-1][target];
+                int notPick = prev[target];
                 int pick = 1e8;
                 if (coins[ind] <= target) {
-                    pick = 1 + dp[ind][target - coins[ind]];
+                    pick = 1 + cur[target - coins[ind]];
                 }
 
-                dp[ind][target] = min(pick, notPick);
+                cur[target] = min(pick, notPick);
             }
+            prev = cur;
         }
 
-        int ans = dp[n-1][amount];
+        int ans = prev[amount];
         return ans >= 1e8 ? -1: ans;
     }
 };

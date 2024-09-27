16cbf2eb85905a5a7b67627eafdd481cbe6ea4fb
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-09-27 23:09:09 +0530

    Time: 44 ms (14.25%) | Memory: 26.2 MB (21.63%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int lcs(int ind1, int ind2, string& str1, string& str2, vector<vector<int>>& dp) {
-        if (ind1 < 0 || ind2 < 0) {
+        if (ind1 == 0 || ind2 == 0) {
             return 0;
         }
 
@@ -9,7 +9,7 @@ public:
             return dp[ind1][ind2];
         }
 
-        if (str1[ind1] == str2[ind2]) {
+        if (str1[ind1-1] == str2[ind2-1]) {
             return 1 + lcs(ind1-1, ind2-1, str1, str2,dp);
         }
 
@@ -18,7 +18,8 @@ public:
 
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size(), m = text2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return lcs(n-1, m-1, text1, text2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+
+        return lcs(n, m, text1, text2, dp);
     }
 };

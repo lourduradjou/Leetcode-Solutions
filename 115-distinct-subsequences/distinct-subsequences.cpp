dead8ca394832b7cd94d168633504b26e7623102
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-10-01 13:31:35 +0530

    Time: 15 ms (77.13%) | Memory: 7.9 MB (94.05%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -3,20 +3,20 @@ public:
     const int MOD = 1e9 + 7;
     int numDistinct(string s1, string s2) {
         int n = s1.size(), m = s2.size();
-        vector<int> prev(m+1, 0), cur(m+1, 0);
+        vector<int> prev(m+1, 0);
 
         prev[0] = 1;
 
         for (int i = 1; i <= n; i++) {
-            cur[0] = 1;
-            for (int j = 1; j <= m; j++) {
+            prev[0] = 1;
+            for (int j = m; j >= 1; j--) {
                 if (s1[i-1] == s2[j-1]) {
-                    cur[j] = (prev[j-1] + prev[j]) % MOD;
+                    prev[j] = (prev[j-1] + prev[j]) % MOD;
                 }else {
-                    cur[j] = prev[j];
+                    prev[j] = prev[j];
                 }    
             }
-            prev = cur;
+            //prev = cur;
         }
 
         return prev[m];

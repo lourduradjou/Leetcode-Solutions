07939d15bb95fb8b3c0d898e74cf111c56ab7646
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-10-01 13:25:42 +0530

    Time: 28 ms (27.54%) | Memory: 25.8 MB (49.16%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int solveNumDistance(int ind1, int ind2, string& s1, string& s2,vector<vector<int>>& dp) {
-        if (ind2 < 0) return 1;
-        if (ind1 < 0) return 0;
-       
-        if (dp[ind1][ind2] != -1) {
-            return dp[ind1][ind2];
+    const int MOD = 1e9 + 7;
+    int numDistinct(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i-1] == s2[j-1]) {
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % MOD;
+                }else {
+                    dp[i][j] = dp[i-1][j];
+                }    
+            }
         }
 
-        if (s1[ind1] == s2[ind2]) {
-            return dp[ind1][ind2] = solveNumDistance(ind1-1, ind2-1, s1, s2, dp) + solveNumDistance(ind1-1, ind2, s1, s2, dp);
-        }
-
-        return dp[ind1][ind2] = solveNumDistance(ind1-1, ind2, s1, s2, dp);
-    }
-    int numDistinct(string s, string t) {
-        int n = s.size(), m = t.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solveNumDistance(n-1, m-1, s, t, dp);
+        return dp[n][m];
     }
 };

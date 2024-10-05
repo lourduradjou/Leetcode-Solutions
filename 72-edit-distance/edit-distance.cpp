411be9d2bf221c073b63089b6bd6302b39c8130b
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-10-05 13:24:54 +0530

    Time: 7 ms (66.77%) | Memory: 10.3 MB (94.57%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,24 +4,26 @@ class Solution {
             int n = word1.size();
             int m = word2.size();
 
-            vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+            vector<int> prev(m+1, 0);
+            vector<int> cur(m+1, 0);
 
             //base cases for tabulation from memo code
-            for (int i = 0; i <= n; i++) dp[i][0] = i;
-            for (int j = 0; j <= m; j++) dp[0][j] = j;
+            for (int j = 0; j <= m; j++) prev[j] = j;
 
             for (int ind1 = 1; ind1 <= n; ind1++) {
+                cur[0] = ind1;
                 for (int ind2 = 1; ind2 <= m; ind2++) {
                     if (word1[ind1-1] == word2[ind2-1]) 
-                        dp[ind1][ind2] = dp[ind1-1][ind2-1]; //matching move back
+                        cur[ind2] = prev[ind2-1]; //matching move back
                     else {
                          //insert, delete, replace operations are there
-                        dp[ind1][ind2] = 1 + min( {dp[ind1][ind2-1], 
-                        dp[ind1-1][ind2], dp[ind1-1][ind2-1]} );
+                        cur[ind2] = 1 + min( {cur[ind2-1], 
+                        prev[ind2], prev[ind2-1]} );
                     } 
                 }
+                prev = cur;
             }
 
-            return dp[n][m];
+            return prev[m];
         }
 };

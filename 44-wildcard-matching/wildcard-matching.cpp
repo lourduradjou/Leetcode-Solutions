08ab0dd4cdd082a0ec846d46237b1a8f46b9bafd
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-10-05 16:51:03 +0530

    Time: 76 ms (17.84%) | Memory: 16.7 MB (62.49%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,35 +1,40 @@
 class Solution {
-private:
-    int findMatch(int ind1, int ind2, string& s1, string& s2,vector<vector<int>>& dp) {
-        if (ind1 == 0 && ind2 == 0) return true;
-        if (ind2 == 0 && ind1 > 0) return false;
-        if (ind1 == 0 && ind2 > 0) {
-            //either all the characters in s1 shd be *(star) else return false 
-            for (int i = 1; i <= ind2; i++) {
-                if (s2[i-1] != '*')
-                    return false;
+    public:
+        bool isMatch(string s1, string s2) {
+            int n = s1.size();
+            int m = s2.size();
+
+            vector<vector<bool>> dp(n+1, vector<bool>(m+1, 0));
+
+            //base cases
+            dp[0][0] = 1;
+            for (int i = 1; i <= n; i++) {
+                dp[i][0] = false;
             }
-            return true;
+            for (int j = 1; j <= m; j++) {
+                dp[0][j] = true;
+                for (int i = 1; i <= j; i++) {
+                    if (s2[i-1] != '*') {
+                        dp[0][j] = false;
+                        break;
+                    }
+                }
+            }
+
+            for (int ind1 = 1; ind1 <= n; ind1++) {
+                for (int ind2 = 1; ind2 <= m; ind2++) {
+                    if (s1[ind1-1] == s2[ind2-1] || s2[ind2-1] == '?') //incase match move back 
+                        dp[ind1][ind2] = dp[ind1 - 1][ ind2 - 1];
+                    else if (s2[ind2-1] == '*') {
+                        //either don't consider the * and move on 
+                        // or consider the * one by one
+                        dp[ind1][ind2] = dp[ind1][ind2 - 1] || dp[ind1 - 1][ind2];
+                    }
+                    else
+                        dp[ind1][ind2] = false;
+                }
+            }
+
+            return dp[n][m];
         }
-
-        if (dp[ind1][ind2] != -1) return dp[ind1][ind2];
-
-        if (s1[ind1-1] == s2[ind2-1] || s2[ind2-1] == '?') //incase match move back 
-            return dp[ind1][ind2] = findMatch(ind1 - 1, ind2 - 1, s1, s2, dp);
-        if (s2[ind2-1] == '*') {
-            //either don't consider the * and move on 
-            // or consider the * one by one
-            return dp[ind1][ind2] = findMatch(ind1, ind2 - 1, s1, s2, dp) || findMatch(ind1 - 1, ind2, s1, s2, dp);
-        }
-
-        return dp[ind1][ind2] = false;
-    }
-public:
-    bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
-
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return findMatch(n, m, s, p, dp);
-    }
 };

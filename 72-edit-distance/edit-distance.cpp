1f378c9fae4460924e63e99294f74ca0224dbc61
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-10-05 13:15:45 +0530

    Time: 9 ms (50.85%) | Memory: 12.7 MB (75.96%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,25 +1,27 @@
 class Solution {
-private:
-    int findMinDistance(int ind1, int ind2, string& str1, string& str2, vector<vector<int>>& dp) {
-        if (ind1 == 0) return ind2;
-        if (ind2 == 0) return ind1;
+    public:
+        int minDistance(string word1, string word2) {
+            int n = word1.size();
+            int m = word2.size();
 
-        if (dp[ind1][ind2] != -1) {
-            return dp[ind1][ind2];
-        }
-        if (str1[ind1-1] == str2[ind2-1]) {
-            return findMinDistance(ind1-1, ind2-1, str1, str2, dp); //matching move back
-        }
-        //insert, delete, replace operations are there
-        return dp[ind1][ind2] = 1 + min( {findMinDistance(ind1, ind2-1,str1, str2, dp), 
-        findMinDistance(ind1-1, ind2,str1, str2, dp), findMinDistance(ind1-1, ind2-1,str1, str2, dp)} );
-    }
-public:
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
+            vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return findMinDistance(n, m, word1, word2, dp);
-    }
+            //base cases for tabulation from memo code
+            for (int i = 0; i <= n; i++) dp[i][0] = i;
+            for (int j = 0; j <= m; j++) dp[0][j] = j;
+
+            for (int ind1 = 1; ind1 <= n; ind1++) {
+                for (int ind2 = 1; ind2 <= m; ind2++) {
+                    if (word1[ind1-1] == word2[ind2-1]) 
+                        dp[ind1][ind2] = dp[ind1-1][ind2-1]; //matching move back
+                    else {
+                         //insert, delete, replace operations are there
+                        dp[ind1][ind2] = 1 + min( {dp[ind1][ind2-1], 
+                        dp[ind1-1][ind2], dp[ind1-1][ind2-1]} );
+                    } 
+                }
+            }
+
+            return dp[n][m];
+        }
 };

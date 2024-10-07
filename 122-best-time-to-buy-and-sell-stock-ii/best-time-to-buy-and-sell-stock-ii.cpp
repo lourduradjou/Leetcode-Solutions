79d4e0a9763071996a254d4c868cbfd468f0ceb3
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-10-07 14:17:34 +0530

    Time: 10 ms (23.71%) | Memory: 20.5 MB (12.69%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,37 +1,35 @@
 class Solution {
 private:
-    static const int BUY = 1;
-    static const int SELL = 0;
-
-    int solve (int i, int can_I_Buy, int &n, vector<int> &prices,
-     vector<vector<int>> &dp) {
-        if (i == n)
+    int func(vector<int> &prices, int ind, int isAllowedToBuy, vector<vector<int>>& dp) {
+        if (ind == prices.size()) 
             return 0;
+        
+        if (dp[ind][isAllowedToBuy] != -1) 
+            return dp[ind][isAllowedToBuy];
 
-        if (dp[i][can_I_Buy] != -1)
-            return dp[i][can_I_Buy];
         int profit = 0;
-
-        if (can_I_Buy == BUY) {
-            //there will be two options where i can buy or i can skip
-            //for this specific i th index
-            int take = -prices[i] + solve(i + 1, SELL, n, prices, dp);
-            int skip = 0 + solve(i + 1, BUY, n, prices, dp);
-            profit = max(take, skip);
-        } 
+        if (isAllowedToBuy) {
+            //we have to choice now, either to buy the stocks and move
+            // or not to buy the stock but move further
+            profit = max( 
+                        (-prices[ind] + func(prices, ind+1, 0, dp)), //bought the stock
+                        (0 +  func(prices, ind+1, 1, dp)) // not bought the stock
+            );
+        }
         else {
-            int sell = prices[i] + solve(i + 1, BUY, n, prices, dp);
-            int notSelled = 0 + solve(i + 1, SELL, n, prices, dp);
-            profit = max(sell, notSelled);
+            profit = max( 
+                        (prices[ind] + func(prices, ind+1, 1, dp)), //sold the stock
+                        (0 +  func(prices, ind+1, 0, dp)) // not sold the stock
+            );
         }
 
-        return dp[i][can_I_Buy] = profit;
-     }
-
+        return dp[ind][isAllowedToBuy] = profit;
+    }
 public:
     int maxProfit(vector<int>& prices) {
+        //using recursion + memoization 
         int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int> (2, -1));
-        return solve( 0, 1, n, prices, dp);
+        vector<vector<int>> dp(n, vector<int> (2, -1));
+        return func(prices, 0, 1, dp); // 1 means allowed to buy, 0 means allowed to sell;
     }
 };

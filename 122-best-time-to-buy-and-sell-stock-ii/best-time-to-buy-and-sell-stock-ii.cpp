a3397ce7d8569285f28f54337b35e42423eed557
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-10-07 15:18:49 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (75.13%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        //using recursion + memoization 
         int n = prices.size();
-        // vector<int> next(2, 0);
-        // vector<int> cur(2, 0);
-        int nextNotTake,nextTake,curNotTake,curTake;
-        nextTake = nextNotTake = 0; //base case , yeah in the tabu itself write that , to understand easily here
-        
-        for (int ind = n-1; ind >= 0; ind--) {
-            curTake = max( 
-                        (-prices[ind] + nextNotTake), //bought the stock
-                        (0 +  nextTake) // not bought the stock
-            );
-            curNotTake = max( 
-                        (prices[ind] + nextTake), //sold the stock
-                        (0 +  nextNotTake) // not sold the stock
-            );
-            nextTake = curTake;
-            nextNotTake = curNotTake;
+        //simple solution is the add up the upward profit arrow, rather then 
+        //considering the negative downward arrows into our profits collection...
+        // simple two plot is enough to understand the logic (witout dp appraoch)
+        int profit = 0;
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > prices[i-1]) {
+                profit += (prices[i] - prices[i-1]);
+            }
         }
-
-        return nextTake; 
+        
+        return profit;
     }
 };

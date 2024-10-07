914b8d736124e858a8f51e81f1f1831afeffb7ef
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-10-07 14:32:53 +0530

    Time: 8 ms (33.49%) | Memory: 18.2 MB (23.40%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,35 +1,32 @@
 class Solution {
-private:
-    int func(vector<int> &prices, int ind, int isAllowedToBuy, vector<vector<int>>& dp) {
-        if (ind == prices.size()) 
-            return 0;
-        
-        if (dp[ind][isAllowedToBuy] != -1) 
-            return dp[ind][isAllowedToBuy];
-
-        int profit = 0;
-        if (isAllowedToBuy) {
-            //we have to choice now, either to buy the stocks and move
-            // or not to buy the stock but move further
-            profit = max( 
-                        (-prices[ind] + func(prices, ind+1, 0, dp)), //bought the stock
-                        (0 +  func(prices, ind+1, 1, dp)) // not bought the stock
-            );
-        }
-        else {
-            profit = max( 
-                        (prices[ind] + func(prices, ind+1, 1, dp)), //sold the stock
-                        (0 +  func(prices, ind+1, 0, dp)) // not sold the stock
-            );
-        }
-
-        return dp[ind][isAllowedToBuy] = profit;
-    }
 public:
     int maxProfit(vector<int>& prices) {
         //using recursion + memoization 
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int> (2, -1));
-        return func(prices, 0, 1, dp); // 1 means allowed to buy, 0 means allowed to sell;
+        vector<vector<int>> dp(n+1, vector<int> (2, 0));
+
+        
+        for (int ind = n-1; ind >= 0; ind--) {
+            for (int isAllowedToBuy = 0; isAllowedToBuy < 2; isAllowedToBuy++) {
+                int profit = 0;
+                if (isAllowedToBuy) {
+                    //we have to choice now, either to buy the stocks and move
+                    // or not to buy the stock but move further
+                    profit = max( 
+                                (-prices[ind] + dp[ind+1][0]), //bought the stock
+                                (0 +  dp[ind+1][1]) // not bought the stock
+                    );
+                }
+                else {
+                    profit = max( 
+                                (prices[ind] + dp[ind+1][1]), //sold the stock
+                                (0 +  dp[ind+1][0]) // not sold the stock
+                    );
+                }
+
+                dp[ind][isAllowedToBuy] = profit;
+            }
+        }
+        return dp[0][1]; // 1 means allowed to buy, 0 means allowed to sell;
     }
 };

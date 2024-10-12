59bb5b275e619ec605709c1a0b482598b60d0842
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-10-12 21:57:43 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (81.61%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -6,24 +6,16 @@ public:
         vector<int> cur(2, 0);
         vector<int> next1(2, 0);
         vector<int> next2(2, 0);
-
-
-        for (int ind = n-1; ind >= 0; ind--) {
-            for (int buy = 1; buy >= 0; buy--) {
-                if (buy) {
-                    //case is to buy , either buy it or move without buying
-                    //buy it
-                    cur[buy] = max(
-                        -prices[ind] + next1[0],
-                        0 + next1[1]
-                        );
-                } else {
-                    cur[buy] = max(
-                        prices[ind] + next2[1],
-                        0 + next1[0]
-                        );
-                }
-            }
+        for (int ind = n-1; ind >= 0; ind--) {     
+            cur[1] = max(
+                -prices[ind] + next1[0],
+                0 + next1[1]
+                );
+    
+            cur[0] = max(
+                prices[ind] + next2[1],
+                0 + next1[0]
+                );  
             next2 = next1;
             next1 = cur;
         }

2acced7240ffe49df646cb99f5ba341f883514b4
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2024-10-28 16:13:31 +0530

    Time: 84 ms (47.67%) | Memory: 13.2 MB (50.13%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -2,22 +2,17 @@ class Solution {
 
 public:
     int lengthOfLIS(vector<int>& nums) {
-        //using dp subsequence pick and not pick pattern this could be solved easily
-        //with the help of storing the previous to get the increasign subsequence
-        int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        vector<int> dp(nums.size(), 1);
 
-        for (int ind = n-1; ind >= 0; ind--) {
-            for (int prev = ind-1; prev >= -1; prev--) {
-                int maxi = 0;
-                maxi = dp[ind + 1][prev+1];
-                if (prev == -1 || nums[ind] > nums[prev]) {
-                    maxi = max( maxi, 1 + dp[ind + 1][ind + 1]);
+        int maxi = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int prev = 0; prev < i; prev++) {
+                if( nums[prev] < nums[i]) {
+                    dp[i] = max(1 + dp[prev], dp[i]);
                 }
-                dp[ind][prev+1] = maxi;
             }
+            maxi = max(maxi, dp[i]);//finally we need the maximum among them      
         }
-
-        return dp[0][0];
+        return maxi;
     }
 };

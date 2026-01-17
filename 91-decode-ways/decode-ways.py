84295139227c048cd2f60e119d2ef1369f1eea3e
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2026-01-17 19:44:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (7.51%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -2,17 +2,13 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         dp = {len(s) : 1} #base case to return in the dp
 
-        def sol(i):
-            if i in dp:
-                return dp[i]
+        for i in range(len(s) - 1, -1, -1):
             if s[i] == "0":
-                return 0
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
 
-            res = sol(i+1)
             if (i + 1) < len(s) and (s[i] == "1" or (s[i] == "2" and s[i+1] in "0123456")):
-                res += sol(i+2)
-
-            dp[i] = res
-            return res
+                dp[i] += dp[i+2]
         
-        return sol(0)
+        return dp[0]

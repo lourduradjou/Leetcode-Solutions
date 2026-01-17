89db71f918dded98a60b7ef24cbc0b798d590cd6
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2026-01-17 19:53:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (12.80%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,14 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = {len(s) : 1} #base case to return in the dp
+        dp1 = 1 # corresponds to dp[n] = 1
+        dp2 = 0 # coresponds to the next variable initialized with zero
 
         for i in range(len(s) - 1, -1, -1):
-            if s[i] == "0":
-                dp[i] = 0
-            else:
-                dp[i] = dp[i + 1]
+            curr = 0
 
-            if (i + 1) < len(s) and (s[i] == "1" or (s[i] == "2" and s[i+1] in "0123456")):
-                dp[i] += dp[i+2]
+            if s[i] != "0":
+                curr = dp1
+                if (i + 1) < len(s) and (s[i] == "1" or (s[i] == "2" and s[i+1] in "0123456")):
+                    curr += dp2
+            
+            dp2 = dp1
+            dp1 = curr
         
-        return dp[0]
+        return dp1
